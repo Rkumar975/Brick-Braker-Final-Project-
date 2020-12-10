15495b503ef82e7be0149549d52c6645dddc8a89
Rkumar975 <72274090+Rkumar975@users.noreply.github.com>
2020-12-09 23:35:24 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brickBraker;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Rachhpal
+ */
+public class Main {
+    public static void main(String[] args){
+       JFrame obj = new JFrame(); 
+       Gameplay gamePlay = new Gameplay();
+       obj.setBounds(10, 10, 700, 600);
+       obj.setTitle("Breakout Ball");
+       obj.setResizable(false);
+       obj.setVisible(true);
+       obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       obj.add(gamePlay);
+    }
+}
